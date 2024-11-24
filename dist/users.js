dccc2da614eb7df6ebaa8c40bcce63fce629adc9
Erik <bergi.rik@gmail.com>
2024-11-24 21:08:36 +0100

    updatet with tsc

--- dist/users.js
+++ dist/users.js
@@ -5,21 +5,6 @@ let user1 = {
     username: "admin",
     password: "admin"
 };
-let user2 = {
-    username: "viki",
-    password: "rose"
-};
-let user3 = {
-    username: "huemer",
-    password: "sytIstToll"
-};
-let user4 = {
-    username: "Gerald",
-    password: "PapaSchlumpf"
-};
 exports.allusers = [
-    user1,
-    user2,
-    user3,
-    user4
+    user1
 ];
